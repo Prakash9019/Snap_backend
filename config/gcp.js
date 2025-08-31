2ca736240219a36034c8c9f03050d3fae65faded
Prakash9019 <plsprakash2003@gmail.com>
2025-08-31 07:44:42 +0530

    added

--- config/gcp.js
+++ config/gcp.js
@@ -1,24 +1,37 @@
+// const { Storage } = require('@google-cloud/storage');
+// const path = require('path');
+
+// // Ensure GOOGLE_APPLICATION_CREDENTIALS is set in your .env or environment
+// // It should point to the path of your service account key file.
+// // Example: GOOGLE_APPLICATION_CREDENTIALS=/path/to/your/keyfile.json
+
+// // Alternatively, you can directly provide the key file content:
+// // const storage = new Storage({
+// //   credentials: JSON.parse(process.env.GCP_KEY_FILE_CONTENT)
+// // });
+
+// const storage = new Storage();
+
+// const bucketName = process.env.GCP_BUCKET_NAME;
+
+// if (!bucketName) {
+//   console.error('GCP_BUCKET_NAME environment variable is not set.');
+//   process.exit(1);
+// }
+
+// const bucket = storage.bucket(bucketName);
+
+// module.exports = { bucket };
+
+
 const { Storage } = require('@google-cloud/storage');
 const path = require('path');
+require('dotenv').config();
 
-// Ensure GOOGLE_APPLICATION_CREDENTIALS is set in your .env or environment
-// It should point to the path of your service account key file.
-// Example: GOOGLE_APPLICATION_CREDENTIALS=/path/to/your/keyfile.json
+const storage = new Storage({
+  keyFilename: path.join(__dirname, '../uploads/serviceAccountKey.json'),
+});
 
-// Alternatively, you can directly provide the key file content:
-// const storage = new Storage({
-//   credentials: JSON.parse(process.env.GCP_KEY_FILE_CONTENT)
-// });
+const bucket = storage.bucket(process.env.GCS_BUCKET_NAME);
 
-const storage = new Storage();
-
-const bucketName = process.env.GCP_BUCKET_NAME;
-
-if (!bucketName) {
-  console.error('GCP_BUCKET_NAME environment variable is not set.');
-  process.exit(1);
-}
-
-const bucket = storage.bucket(bucketName);
-
-module.exports = { bucket };
+module.exports = bucket;
